ecf29cb17694be3cf22edacff12dc2a8e16cadbf
Wesley Tam <hcwtam@gmail.com>
2020-05-27 22:36:06 +0800

    create scroll project

---
+++ 013-scroll/app.js
@@ -0,0 +1,18 @@
+// Set date
+const date = document.querySelector('#date');
+date.innerHTML = new Date().getFullYear();
+
+// Close links
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click',()=> {
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+})
