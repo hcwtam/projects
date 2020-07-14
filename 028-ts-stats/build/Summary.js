d6587b08df37521fa078c119a6e351bd4a425e9c
Wesley Tam <hcwtam@gmail.com>
2020-07-14 19:04:54 +0800

    learn about enum, tuple as type definition, generic, inheritence vs composition and composition is preferred

---
+++ 028-ts-stats/build/Summary.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var Summary = /** @class */ (function () {
+    function Summary(analyzer, outputTarget) {
+        this.analyzer = analyzer;
+        this.outputTarget = outputTarget;
+    }
+    Summary.winsAnalysisWithHtmlReport = function (team) {
+        return new Summary(new WinsAnalysis_1.WinsAnalysis(team), new ConsoleReport_1.ConsoleReport());
+    };
+    Summary.prototype.buildAndPrintReport = function (matches) {
+        var output = this.analyzer.run(matches);
+        this.outputTarget.print(output);
+    };
+    return Summary;
+}());
+exports.Summary = Summary;
